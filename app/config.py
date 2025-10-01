dc0dc723ca240401c5e6808bea027a3af8b1b737
Hanush <112359676+hanush21@users.noreply.github.com>
2025-10-01 10:02:30 +0200

    change config

--- app/config.py
+++ app/config.py
@@ -1,11 +1,12 @@
 import mysql.connector
 from mysql.connector import Error
+import os
 
 mydb = mysql.connector.connect(
-    host="",
-    user="",
-    password="",
-    database=""
+    host=os.getenv("DB_HOST", "localhost"),
+    user=os.getenv("DB_USER", "root"),
+    password=os.getenv("DB_PASSWORD", ""),
+    database=os.getenv("DB_NAME", "carreras_db")
 )
 
 print(mydb)
